968d34393803e88bea3e8f19a87f7e7b9fbebdeb
Beatriz Rizental <beatriz.rizental@gmail.com>
2017-12-21 22:06:17 -0200

    Adds diferent build script for deploy to npm.
    - The current script added the main.js file, which is a test file
    and makes no sense to add it to the deployed code.

---
+++ webpack-prod.config.js
@@ -0,0 +1,52 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const extractSass = new ExtractTextPlugin({
+  filename: "./dist/css/main.min.css"
+});
+const uglifyJS = new UglifyJsPlugin({
+  sourceMap: true
+})
+
+module.exports = {
+  entry: "./lib/js/prod.js",
+  output: {
+    filename: "./dist/js/bundle.min.js"
+  },
+  devtool: "source-map",
+  stats: git    colors: true,
+    reasons: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+          use: extractSass.extract({
+            use: [{
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+                outputStyle: "compressed"
+              }
+            }]
+          })
+      }
+    ]
+  },
+  plugins: [
+    extractSass,
+    uglifyJS
+  ]
+};
+ extractSass
+  ]
+};
